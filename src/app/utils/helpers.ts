1335ac3b9038d020ef49fab218802dbe3709d38e
okpalaChidiebere <okpalacollins4@gmail.com>
2022-09-20 14:22:37 -0600

    Starter Code

---
+++ src/app/utils/helpers.ts
@@ -0,0 +1,49 @@
+export type FormatedTweet = {
+  name: String;
+  id: String;
+  timestamp: String;
+  text: String;
+  avatar: String;
+  likes: Number;
+  replies: Number;
+  hasLiked: Boolean;
+  parent: null | {
+    author: string;
+    id: string;
+  };
+};
+
+//takes in a timestamp formats it and returns formatted timestamp
+export function formatDate(timestamp) {
+  const d = new Date(timestamp);
+  const time = d.toLocaleTimeString('en-US');
+  return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString();
+}
+
+//takes in an information about a new tweet and formats it in a way that suits our Redux store
+export function formatTweet(
+  tweet,
+  author,
+  authedUser,
+  parentTweet
+): FormatedTweet {
+  const { id, likes, replies, text, timestamp } = tweet;
+  const { name, avatarURL } = author;
+
+  return {
+    name,
+    id,
+    timestamp,
+    text,
+    avatar: avatarURL,
+    likes: likes.length,
+    replies: replies.length,
+    hasLiked: likes.includes(authedUser),
+    parent: !parentTweet
+      ? null
+      : {
+          author: parentTweet.author,
+          id: parentTweet.id,
+        },
+  };
+}

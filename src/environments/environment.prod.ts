a3b89237b5b6ec5cbd87e88fce6e9c8866b2809e
okpalaChidiebere <okpalacollins4@gmail.com>
2023-04-28 12:06:18 -0600

    Update env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
   appName: 'ng-chirper-app',
-  //loadBalancer url or public custom domain name link to the loadBalancer
-  httpApiHost: '',
-  http2ApiHost: '',
+  /**
+   * loadBalancer url or public custom domain name link to the loadBalancer
+   */
+  httpApiHost: 'https://apigateway-1.laundrykya.com',
+  http2ApiHost: 'https://apigateway-2.laundrykya.com',
 };
